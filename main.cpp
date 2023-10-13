7233310e2ad7f9bb0944ad4e93c4df6d468e753d
Michail <bmichailabc@gmail.com>
2023-10-13 10:57:06 +0200

    Коммит 2

--- main.cpp
+++ main.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
-// using m
+// using namespace std;
+
+using std::endl;
+using std::cout;
+
+int main() {
+    int long a;
+    std::cin>> a;
+    cout<<a<<endl;
+
+    return 0;
+}
